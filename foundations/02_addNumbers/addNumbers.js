bf5ee20d63e00bbe12cfdc85a0d1548aca686c33
karl-raymon <devraymon31@gmail.com>
2026-01-20 16:16:54 +0800

    Fixed return issue: removed '' that wraps the variable

--- foundations/02_addNumbers/addNumbers.js
+++ foundations/02_addNumbers/addNumbers.js
@@ -4,10 +4,14 @@ function addNumbers() {
 
   let result;
 
-  result = "a" + "b"; // <------ EDIT THIS LINE
+  result = a + b; // <------ EDIT THIS LINE
 
   return result;
 }
 
 // Do not change this
 module.exports = addNumbers;
+
+//Troubleshoot and modify the existing function so that it returns the digit 2. Avoid "hard coding" the result and try to use variables `a` and `b` to get the correct output.
+
+//Currently, it is not returning the correct value.
