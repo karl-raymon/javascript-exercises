56a9332884d56c00cf57a111377026aab870ae0e
karl-raymon <devraymon31@gmail.com>
2026-01-29 23:01:25 +0800

    Create a function that repeats String with the given number

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  let arr = [];
+  if (num < 0) return 'ERROR';
+  for (let i = 0; i < num; i++) {
+    arr.push(str);
+  }
+  return arr.join('');
 };
 
 // Do not edit below this line
