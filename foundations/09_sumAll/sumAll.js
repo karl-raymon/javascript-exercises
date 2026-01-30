58aaa46737b620c81f4ea509d5cb276d653b016e
karl-raymon <devraymon31@gmail.com>
2026-01-30 22:14:27 +0800

    Created a function that returns the sum of all integers between two numbers

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (...args) {
+  args.sort((a, b) => a - b);
+  let sum = 0;
 
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] < 0 || Math.floor(args[i]) !== args[i]) {
+      return 'ERROR';
+    }
+  }
+  //   if (args.some((item) => item < 0)) return 'Error';
+
+  for (let i = args[0]; i <= args[args.length - 1]; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
