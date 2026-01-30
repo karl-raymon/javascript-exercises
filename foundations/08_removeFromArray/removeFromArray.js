610091fab50890f01d155741a570f21dabc8aef7
karl-raymon <devraymon31@gmail.com>
2026-01-30 20:13:27 +0800

    Made a function that removes a number from the given Array

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...params) {
+  let newArr = [];
+  arr.map((item) => {
+    if (!params.includes(item)) {
+      newArr.push(item);
+    }
+  });
+  return newArr;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// npm test removeFromArray.spec.js
