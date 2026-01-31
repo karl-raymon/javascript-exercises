e2c84831a697dd1c5a1909bddf1c2a77ffa41a40
karl-raymon <devraymon31@gmail.com>
2026-01-31 19:50:12 +0800

    Created a function that converts celsius - fahrenheit and vice versa

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (far) {
+  let cel = 0;
+  cel = Number(((far - 32) * (5 / 9)).toFixed(1));
+  return cel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cel) {
+  let far = 0;
+  far = Number((cel * (9 / 5) + 32).toFixed(1));
+  return far;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
