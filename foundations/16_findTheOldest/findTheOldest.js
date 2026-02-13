dc5278966b4435532fc91d971f56c88dc0204965
karl-raymon <devraymon31@gmail.com>
2026-02-14 02:52:47 +0800

    Created a function where it returns thr oldest person

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  const newArr = arr.sort((a, b) => {
+    const prev = (a.yearOfDeath ?? new Date().getFullYear()) - a.yearOfBirth;
+    const curr = (b.yearOfDeath ?? new Date().getFullYear()) - b.yearOfBirth;
 
+    return prev - curr;
+  });
+  return newArr[newArr.length - 1];
 };
 
 // Do not edit below this line
