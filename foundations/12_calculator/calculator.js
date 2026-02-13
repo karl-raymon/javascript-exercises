7844bfd32cd01d1987c7b9bb51a27f2f6ba50229
karl-raymon <devraymon31@gmail.com>
2026-02-13 19:46:35 +0800

    Create function for a simple calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.length === 0 ? 0 : arr.reduce((a, b) => a + b);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((a, b) => a * b);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  // let tempTotal = 0;
+  if (a === 0) {
+    return 1;
+  }
+  const arr = [];
+  for (let i = 1; i <= a; i++) {
+    arr.push(i);
+  }
+  return arr.reduce((a, b) => a * b);
 };
 
 // Do not edit below this line
@@ -29,5 +37,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
