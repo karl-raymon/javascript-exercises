2aec55b1da376b353c79b41d9654fa4e30eca938
karl-raymon <devraymon31@gmail.com>
2026-02-14 01:47:46 +0800

    Created a function gets the value of a given number in fibonacci sequence

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  let temp = 1;
+  //   let sum = 0;
+  let arr = [];
+  if (num < 0) {
+    return 'OOPS';
+  } else if (num == 0) {
+    return 0;
+  }
+  for (let i = 0; i < num; i++) {
+    if (arr.length === 0 || arr.length === 1) {
+      arr.push(temp);
+    } else {
+      arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+    }
+  }
 
+  //   for (let j = 0; j < arr.length; j++) {
+  //     sum += arr.length();
+  //   }
+  return arr[arr.length - 1];
 };
 
 // Do not edit below this line
