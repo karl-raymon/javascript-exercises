e3d56066ba66518f7e640b9aaa4b161e85aeefe8
karl-raymon <devraymon31@gmail.com>
2026-02-13 20:42:37 +0800

    Made a function that checks if the word is a pallindrome use regex to remove non alphanumeric characters

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  let cleanedWord = word.replace(/[^a-z0-9]/gi, '').toLowerCase();
+  let reversedWord = cleanedWord.split('').reverse().join('');
 
+  return cleanedWord === reversedWord;
 };
 
 // Do not edit below this line
